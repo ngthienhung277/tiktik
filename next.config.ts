61d4d789f57f39bbc7aae370f7e94090d54406ce
Hung Nguyen <ngthienhung277@gmail.com>
2025-07-04 17:28:36 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'encrypted-tbn0.gstatic.com'
+    ],
+  }
 };
 
 export default nextConfig;
